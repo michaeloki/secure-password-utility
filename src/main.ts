0abbbc737356a81bb5fe0d3267ca32c3be2e8ae9
Michael Oki <mikeoki.oki@gmail.com>
2022-02-12 21:21:42 +0200

    Fixed JS compatibility issue

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,39 @@
-const allConstants = require('./constants');
+const allConstants = require('./constants.ts');
 
-function WeakPasswordChecker(rawPassword, passwordLength) {
-    let status = true;
+module.exports = {
+    WeakPasswordChecker: function (rawPassword, passwordLength) {
+        let status = true;
 
-    try {
-        if (!rawPassword || !passwordLength) {
-            return !status;
-        } else {
-            try {
-                if (!(passwordLength === rawPassword.length && passwordLength >= 10)) {
-                    return !status;
-                } else {
-                    allConstants.getCommonWords().forEach((word) => {
-                        if (rawPassword.includes(word)) {
-                            status = false;
+        try {
+            if (!rawPassword || !passwordLength) {
+                return !status;
+            } else {
+                try {
+                    if (!(passwordLength === rawPassword.length && passwordLength >= 10)) {
+                        return !status;
+                    } else {
+                        allConstants.getCommonWords().forEach((word) => {
+                            if (rawPassword.includes(word)) {
+                                status = false;
+                            }
+                        });
+
+                        if (status) {
+                            // check if it contains special characters, digit, small and capital letters
+                            const regExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+                            if (!rawPassword.match(regExp)) {
+                                status = false;
+                            }
                         }
-                    });
-
-                    if (status) {
-                        // check if it contains special characters, digit, small and capital letters
-                        const regExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-                        if (!rawPassword.match(regExp)) {
-                            status = false;
-                        }
+                        return status;
                     }
-                    return status;
+                } catch (e) {
+                    console.log("SecurePasswordUtility::: ", e.message);
                 }
-            } catch (e) {
-                console.log("SecurePasswordUtility::: ", e.message);
             }
+        } catch (e) {
+            console.log("SecurePasswordUtility::: ", e.message);
+            return status;
         }
-    } catch (e) {
-        console.log("SecurePasswordUtility::: ", e.message);
-        return status;
     }
 }
