458c0ecfebf1233c756cfe9bc6f0223d7959f274
Michael Oki <mikeoki.oki@gmail.com>
2022-02-21 13:10:31 +0200

    Updated README.MD

--- tests/test.ts
+++ tests/test.ts
@@ -2,6 +2,8 @@ import {assert, expect} from "chai";
 
 let passwordLength = 13;
 let rawPassword = "Turpen73tino!";
+let productKeyLengthA = 16;
+let productKeyLengthB = 20;
 
 describe('StringChecker', function () {
     it('confirm', function () {
@@ -21,5 +23,17 @@ describe('CreateStrongPassword', function () {
     it('compare', function () {
         let result = rawPassword.length == passwordLength;
         assert.ok(typeof result,"string");
+    });
+});
+
+describe('ProductKeyGenerator', function () {
+    it('compare', function () {
+        let inputA = productKeyLengthA%4;
+        let inputB = productKeyLengthB%5;
+        // expect(result).equal(true);
+
+        expect( (inputA || inputB)).equal( 0);
+            //.equal(true);
+        expect(productKeyLengthA >= 16).equal(true);
     });
 });
